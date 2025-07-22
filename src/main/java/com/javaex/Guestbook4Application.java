bb9f72015943999265de91ca5e4cf098b323bfe6
clz2025.red <clz2025-red@gmail.com>
2025-07-23 00:42:57 +0900

    .

--- src/main/java/com/javaex/Guestbook4Application.java
+++ src/main/java/com/javaex/Guestbook4Application.java
@@ -2,19 +2,12 @@ package com.javaex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Guestbook4Application extends SpringBootServletInitializer {
+public class Guestbook4Application  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Guestbook4Application.class, args);
-	}
-
-	@Override // 이 메서드를 추가
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Guestbook4Application.class);
 	}
 
 }
