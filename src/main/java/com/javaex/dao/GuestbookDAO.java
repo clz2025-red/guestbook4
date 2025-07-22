d83ce101e23888944c2a375c9d9635253250b64a
clz2025-red <clz2025.red@gmail.com>
2025-07-22 19:17:03 +0900

    2025-07-22

---
+++ src/main/java/com/javaex/dao/GuestbookDAO.java
@@ -0,0 +1,48 @@
+package com.javaex.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.GuestbookVO;
+
+@Repository
+public class GuestbookDAO {
+
+	// 필드
+	@Autowired
+	private SqlSession sqlSession;
+	
+	
+	// 전체데이터 가져오기
+	public List<GuestbookVO> guestbookSelect() {
+		System.out.println("GuestbookDAO.guestbookSelect()");
+		
+		List<GuestbookVO> guestbookList = sqlSession.selectList("guestbook.selectList");
+		
+		return guestbookList;
+	}
+	
+
+	// 저장하기
+	public int guestbookInsert(GuestbookVO guestbookVO) {
+		System.out.println("GuestbookDAO.guestbookInsert()");
+		
+		int count = sqlSession.insert("guestbook.insert", guestbookVO);
+		
+		return count;
+	}
+
+	
+	// 삭제하기
+	public int guestbookDelete(GuestbookVO guestbookVO) {
+		System.out.println("GuestbookDAO.guestbookDelete()");
+		
+		int count = sqlSession.delete("guestbook.delete", guestbookVO);
+		
+		return count;
+	}
+
+}
